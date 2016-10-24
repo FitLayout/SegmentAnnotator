66b27f847e88b1c04296872b2004f3e7337c98fa
Radek Burget <burgetr@fit.vutbr.cz>
2016-10-24 21:52:59 +0200

    Grouping operator base

---
+++ src/main/java/org/fit/layout/sa/op/GroupByExampleOperator.java
@@ -0,0 +1,74 @@
+/**
+ * GroupByExampleOperator.java
+ *
+ * Created on 24. 10. 2016, 21:32:40 by burgetr
+ */
+package org.fit.layout.sa.op;
+
+import org.fit.layout.impl.BaseOperator;
+import org.fit.layout.model.Area;
+import org.fit.layout.model.AreaTree;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author burgetr
+ */
+public class GroupByExampleOperator extends BaseOperator
+{
+    private static Logger log = LoggerFactory.getLogger(GroupByExampleOperator.class);
+
+    protected final String[] paramNames = { };
+    protected final ValueType[] paramTypes = { };
+    
+    public GroupByExampleOperator()
+    {
+    }
+    
+    @Override
+    public String getId()
+    {
+        return "FitLayout.Segm.GroupByExample";
+    }
+    
+    @Override
+    public String getName()
+    {
+        return "Group by example";
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "..."; //TODO
+    }
+
+    @Override
+    public String[] getParamNames()
+    {
+        return paramNames;
+    }
+
+    @Override
+    public ValueType[] getParamTypes()
+    {
+        return paramTypes;
+    }
+    
+    //==============================================================================
+
+    @Override
+    public void apply(AreaTree atree)
+    {
+    }
+
+    @Override
+    public void apply(AreaTree atree, Area root)
+    {
+    }
+    
+    //==============================================================================
+    
+    
+}
